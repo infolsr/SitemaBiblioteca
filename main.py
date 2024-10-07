21662f7b1dcd4fba8259a5f58c74ea9907a98e4b
Luis Gonzalez Castro <infolsr@gmail.com>
2024-10-07 20:44:22 -0300

    Creación clase libro

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #prueba roberto
 #prueba fernando
 #prueba luis
+
+class libro:
+    def __init__(self, id_libro, titulo, autor):
+        self.id_libro = id_libro
+        self.titulo = titulo
+        self.autor = autor
+        self.prestamo = False
