f36c5f9473258434e78e8dd435c9eb32d1c6c45e
Luis Gonzalez Castro <infolsr@gmail.com>
2024-10-08 08:35:04 -0300

    metodos de clases usuario y prestamo

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 #prueba fernando
 #prueba luis
 
-class libro:
+from datetime import date
+
+class Libro:
     def __init__(self, id_libro, titulo, autor):
         self.id_libro = id_libro
         self.titulo = titulo
@@ -21,24 +23,60 @@ class libro:
         else:
             print("el libro no está prestado")
 
-class usuario:
-    def __init__(self, id_usuario,nombre,):
+class Usuario:
+    def __init__(self, id_usuario,nombre):
         self.id_usuario = id_usuario
         self.nombre = nombre
+        self.libros_prestados = []
     
     def pedirLibro(self,libro):
         if libro.prestamo == True:
             print (f"{libro.titulo} esta prestado")
         else:
-            Prestamo = prestamo(libro)
-            Prestamo = realizarPrestamo
+            prestamo = Prestamo(self,libro)
+            prestamo.realizarPrestamo()
+            self.libros_prestados.append(libro)
+            print(f"{self.nombre} a pedido prestado el libro {libro.titulo}")
     
+    def devolver_libro(self,libro):
+        if libro in self.libros_prestados:
+            libro.devolverlibro()
+            self.libros_prestados.remove(libro)  # Retiramos el libro de la lista de préstamos
+            print(f"{self.nombre} ha devuelto el libro '{libro.titulo}'.")
+        else:
+            print(f"{self.nombre} no tiene el libro '{libro.titulo}' en préstamo.")
 
-class prestamo:
+class Prestamo:
     def __init__(self, usuario, libro):
         self.usuario = usuario
         self.libro = libro
         self.fecha_prestamo = None
         self.ocupado = False
 
+    def realizarPrestamo(self):
+        if not self.ocupado:
+            self.libro.prestarlibro()
+            self.fecha_prestamo = date.today()  # Establecemos la fecha del préstamo
+            self.ocupado = True
+            print(f"Préstamo realizado: {self.libro.titulo} prestado a {self.usuario.nombre} el {self.fecha_prestamo}.")
+        else:
+            print(f"El préstamo ya está activo.")
 
+    def finalizar_prestamo(self):
+        if self.ocupado:
+            self.libro.devolverlibro()
+            self.ocupado = False
+            print(f"Préstamo finalizado: {self.libro} devuelto por {self.usuario}.")
+        else:
+            print("No hay préstamo activo.")
+
+
+libro1 = Libro(1, "1984", "George Orwell")
+libro2 = Libro(2, "Cien años de soledad", "Gabriel García Márquez")
+usuario1 = Usuario(1, "Juan Pérez")
+usuario2 = Usuario(2, "Ana López")
+
+usuario1.pedirLibro(libro1) 
+usuario2.pedirLibro(libro1)
+usuario1.devolver_libro(libro1)  # Juan devuelve "1984"
+usuario2.pedirLibro(libro1)  # Ana ahora puede pedir "1984"
