99ed98d3e0a72f3328a1bb46a602ac2a2e4a05d2
Luis Gonzalez Castro <infolsr@gmail.com>
2024-10-07 21:14:38 -0300

    metodos libro agregar y devolver

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ class libro:
         self.titulo = titulo
         self.autor = autor
         self.prestamo = False
-        
+
+    def prestarlibro(self):
+        if not self.prestamo: #si no es verdadero prestar
+            self.prestamo =True # cambia estado a prestado
+        else:
+            print("El libro ya está prestado")
+
+    def devolverlibro(self):
+        if self.prestamo: 
+            self.prestamo =False
+        else:
+            print("el libro no está prestado")
 
 class usuario:
     def __init__(self, id_usuario,nombre,):
